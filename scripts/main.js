4bebb504d335e1ca48898909c1ad89ca2c6406de
윤성현 <oko9495@naver.com>
2024-05-19 19:43:31 +0900

    2024-05-16

--- scripts/main.js
+++ scripts/main.js
@@ -20,6 +20,14 @@ play_button.addEventListener("click", play);
 function play() {
   let user_input_num = user_num.value;
 
+  if (user_input_num < 0 || user_input_num > 100) {
+    result_area.textContent = '1부터 100까지 숫자만 입력하시오';
+    return
+  } else if (user_input_num == '') {
+    result_area.textContent = '숫자를 입력해 주세요'
+    return
+  }
+
   life_num--;
   console.log(life_num);
 
@@ -33,12 +41,33 @@ function play() {
 
   if (life_num < 1) {
     game_over = true;
-    life_area.textContent = '끝';
+    life_area.textContent = "끝";
   } else {
     life_area.textContent = "남은 찬스 : " + life_num + "번";
   }
-  
+
   if (game_over == true) {
     play_button.disabled = true;
   }
 }
+
+// 리셋 버튼 눌렀을때 리렛
+
+let reset_button = document.getElementById("reset_button");
+
+reset_button.addEventListener("click", reset);
+
+function reset() {
+  random_num();
+  game_over = false;
+  play_button.disabled = false;
+  life_num = 5;
+  life_area.textContent = "남은 찬스 : 5번";
+}
+
+
+// user_num 커서 표시일때 초기화
+
+user_num.addEventListener('mouseenter', function(){
+  user_num.value = '';
+});
